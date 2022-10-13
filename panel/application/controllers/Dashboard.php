92bc33e1a4b582f76c61700fb6fdbed2f0428cc6
SelimTekin <tekinselim.57.57@gmail.com>
2022-10-13 14:31:55 +0300

    View yapisi tek klasor içine atildi. ViewFolder eklendi

---
+++ panel/application/controllers/Dashboard.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public $viewFolder = "";
+
+	public function __construct(){ # bu class çağırıldığında ilk çalıştırılacak olan metottur.
+		parent::__construct(); #CI_Controller class'ı içindeki construct metodunu çalıştırmak için
+
+		$this->viewFolder = "dashboard_v";
+	}
+
+	public function index()
+	{
+		$this->load->view("{$this->viewFolder}/index");
+	}
+}

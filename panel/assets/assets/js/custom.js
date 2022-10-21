ab49917d8b0041adc1dad6bdda35b1678297c522
SelimTekin <tekinselim.57.57@gmail.com>
2022-10-21 17:54:55 +0300

    SweetAlert ile Confirm kullanilarak Kayit silme işlemi gerceklestirildi

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
-    Swal.fire({
-        title: 'Error!',
-        text: 'Do you want to continue',
-        icon: 'error',
-        confirmButtonText: 'Cool'
-    })
+
+    $(".remove-btn").click(function(e){
+
+        var $data_url = $(this).data("url"); // başında data- olan attributeleri keywordleri buluyor
+
+        Swal.fire({
+            title: 'Emin misiniz?',
+            text: "Bu işlemi geri alamayacaksınız!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Evet, Sil!',
+            cancelButtonText: 'Hayır'
+          }).then(function(result){
+            if (result.isConfirmed) {
+              window.location.href = $data_url;
+            }
+          });
+    });
+
+    // Swal.fire({
+    //     title: 'Error!',
+    //     text: 'Do you want to continue',
+    //     icon: 'error',
+    //     confirmButtonText: 'Cool'
+    // })
 })

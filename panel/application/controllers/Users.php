cc24a874c289dadd3ab31f55a300e53ca5051074
SelimTekin <tekinselim.57.57@gmail.com>
2022-11-10 22:21:20 +0300

    Kullanici ekleme, duzenleme ve silme islemleri tamamlandi

--- panel/application/controllers/Users.php
+++ panel/application/controllers/Users.php
@@ -149,16 +149,54 @@ class Users extends CI_Controller{ # CI -> CodeIgniter (extend etmemizin sebebi 
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
     }
 
+    public function update_password_form($id){
+
+        $viewData = new stdClass();
+
+        // Tablodan verilerin getirilmesi
+        $item = $this->user_model->get(
+            array(
+                "id" => $id,
+            )
+        );
+
+        // view'e gönderilecek değişkenlerin set edilmesi
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "password";
+        $viewData->item = $item;
+
+        # ikinci parametre olan $viewData'yı bu view'e gönderelim ki viewFolder ve subViewFolder'ı index sayfasında kullanabilelim
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+    }
+
     public function update($id){
 
         $this->load->library("form_validation");
 
-        // Kurallar yazilir
-        $this->form_validation->set_rules("title", "Başlık", "required|trim"); # input'un name'i, kural'ın(rule) ismi, kurallar (trim başındaki ve sonundaki boşlukları kontrol eder)
+        $oldUser = $this->user_model->get(
+            array(
+                "id" => $id
+            )
+        );
 
+        if($oldUser->user_name != $this->input->post("user_name")){
+            $this->form_validation->set_rules("user_name", "Kullanıcı Adı", "required|trim|is_unique[users.user_name]"); # is_unique -> benzersiz mi diye kontrol eder. Mesela aynı kullanıcı adından bir tane daha var mı diye kontrol eder. users.user_name -> users tablosundaki user_name alanını temsil eder
+        }
+
+        if($oldUser->user_name != $this->input->post("email")){
+            $this->form_validation->set_rules("email", "E-posta", "required|trim|valid_email[users.email]"); # Girilen e-posta adresinin geçerli olup olmadığını kontrol eder
+        }
+
+        // Kurallar yazilir
+        $this->form_validation->set_rules("full_name", "Ad Soyad", "required|trim");
+       
         $this->form_validation->set_message(
             array(
-                "required" => "<b>{field}</b> alanı doldurulmalıdır" # field kuralın adına denk geliyor (örneğin yukarıdaki kuralın adı olan "Başlık")
+                "required"    => "<b>{field}</b> alanı doldurulmalıdır", # field kuralın adına denk geliyor (örneğin yukarıdaki kuralın adı olan "Başlık")
+                "valid_email" => "Lütfen geçerli e-posta adresi giriniz", 
+                "is_unique"   => "<b>{field}</b> alanı daha önceden kullanılımş",
+                "min_length"  => "En az 6 karakterden oluşan şifre girmelisiniz",
+                "max_length"  => "En fazla 8 karakterden oluşan şifre girmelisiniz",
             )
         );
 
@@ -173,69 +211,16 @@ class Users extends CI_Controller{ # CI -> CodeIgniter (extend etmemizin sebebi 
 
         if($validate){
 
-            // Upload Süreci...
+            $update = $this->user_model->update(
+                array("id" => $id),
 
-            if($_FILES["img_url"]["name"] !== ""){
+                array(
+                    "user_name" => $this->input->post("user_name"),
+                    "full_name" => $this->input->post("full_name"),
+                    "email"     => $this->input->post("email"),
+                )
 
-                // content dosyasındaki görsel inputunun name'i img_url olduğu için img_url yazdık. Yoksa dropzone'da file olarak gönderiliyordu.
-                $file_name = convertToSEO(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
-
-                # uzantı ve dosya ismini ayrı ayrı almak istersek aşağıda yazıyor
-                // $ext = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION); # uzantıyı aldık
-                // $file_name = convertToSEO(pathinfo($_FILES["file"]["name"], PATHINFO_FILENAME)); # dosya ismini uzantısız aldık
-
-                // Bunlar ayar( konfigürasyon(config) ) oluyor
-                // $config["allowed_types"] = "*"; # bütün tipler veya
-                $config["allowed_types"] = "jpg|jpeg|png"; # hangi türde dosyayı yükleyeceğimiz(yazarken aralarda boşluk bırakmadan yaz)
-                $config["upload_path"] = "uploads/$this->viewFolder/"; # Dosyalar nereye yüklencek
-                $config["file_name"] = $file_name;
-
-                # upload sınıfını yüklerken nasıl yüklemek istediğimizi ya da ne şartlarda ya da nereye yükleyeceğizmizi belirttik $config ile
-                $this->load->library("upload", $config);
-
-                // upload sınıfındaki do_upload metodunu kullanarak bu işlemi(upload işlemini) gerçekleştirme
-                $upload = $this->upload->do_upload("img_url"); # Bizim inputumuzun name'i img_url olduğu için img_url yazdık.Neyi upload edeceğini dropzone'dan kaynaklı varsayılan olarak ismi(name) file olarak geliyor.
-
-                if($upload){
-
-                    # upload edilen dosya ile ilgili bilgilerin arasındaki ismini alabiliriz. data dediğimiz zaman array döndürür
-                    $uploaded_file = $this->upload->data("file_name");
-
-                    $data = array(
-                            "title"       => $this->input->post("title"),
-                            "description" => $this->input->post("description"),
-                            "url"         => convertToSEO($this->input->post("title")),
-                            "img_url"   => $uploaded_file,
-                        );
-
-                }
-                else{
-
-                    $alert = array(
-                        "title"  => "İşlem Başarısız",
-                        "text"   => "Görsel yüklenirken bir problem oluştu",
-                        "type"   => "error"
-                    );
-
-                    $this->session->set_flashdata("alert", $alert);
-
-                    redirect(base_url("references/update_form/$id"));
-
-                    die();
-
-                }
-            }
-            else{
-
-                $data = array(
-                    "title"       => $this->input->post("title"),
-                    "description" => $this->input->post("description"),
-                    "url"         => convertToSEO($this->input->post("title")),
-                );
-
-            }
-
-            $update = $this->user_model->update(array("id" => $id), $data);
+            );
 
             if($update){
 
@@ -262,7 +247,7 @@ class Users extends CI_Controller{ # CI -> CodeIgniter (extend etmemizin sebebi 
             $this->session->set_flashdata("alert", $alert); # alert isimli bir indisim var ve bu alert isimli indisimin değeri $alert değişkeninden gelecek
 
             # if'in içinde değil de burada redirect etmemizin sebebi alert'e verileri gönderebilmek. if'in içinde oslaydı session satırına ulaşamadan sayfa redirect edilecekti.
-            redirect(base_url("references"));
+            redirect(base_url("users"));
 
         }
         else{
@@ -272,6 +257,90 @@ class Users extends CI_Controller{ # CI -> CodeIgniter (extend etmemizin sebebi 
             // view'e gönderilecek değişkenlerin set edilmesi
             $viewData->viewFolder = $this->viewFolder;
             $viewData->subViewFolder = "update";
+            $viewData->form_error = true;
+
+            // Tablodan verilerin getirilmesi
+            $viewData->item = $this->user_model->get(
+                array(
+                    "id" => $id,
+                )
+            );            
+
+            # ikinci parametre olan $viewData'yı bu view'e gönderelim ki viewFolder ve subViewFolder'ı index sayfasında kullanabilelim
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
+    }
+
+    public function update_password($id){
+
+        $this->load->library("form_validation");
+
+        // Kurallar yazilir
+        $this->form_validation->set_rules("password", "Şifre", "required|trim|min_length[6]|max_length[8]"); # min ve max girilecek olan karakter sayısını belirliyoruz
+        $this->form_validation->set_rules("re_password", "Şifre", "required|trim|min_length[6]|max_length[8]|matches[password]"); # tekrar girilen şifre password(ilki) ile eşleşiyor mu
+       
+        $this->form_validation->set_message(
+            array(
+                "required"    => "<b>{field}</b> alanı doldurulmalıdır", # field kuralın adına denk geliyor (örneğin yukarıdaki kuralın adı olan "Başlık")
+                "matches"     => "Şifreler birbiriyle uyuşmuyor"
+            )
+        );
+
+        // Form validation calistirilir
+        // TRUE - FALSE
+        $validate = $this->form_validation->run();
+
+        // Basarili ise
+            // Kayit islemi baslar
+        // Basarisiz ise
+            // Hata ekranda gosterilir...
+
+        if($validate){
+
+            $update = $this->user_model->update(
+                array("id" => $id),
+
+                array(
+                    "password" => md5($this->input->post("password")),
+                )
+
+            );
+
+            if($update){
+
+                $alert = array(
+                    "title"  => "Güncelleme Başarılı",
+                    "text"   => "Şifreniz başarılı bir şekilde güncellendi",
+                    "type"   => "success"
+                );
+
+            }
+            else{
+
+                $alert = array(
+                    "title"  => "Güncelleme Başarısız",
+                    "text"   => "Şifre güncelleme sırasında bir problem oluştu",
+                    "type"   => "error"
+                );
+
+            }
+
+            # İşlemin Sonucunu Session'a yazma işlemi...
+            # session'a bir şey eklediğin zaman session'dan onu kaldırmadığın sürece session'da kalacaktır.
+            # set_flashdata() ise bir kere set edilecek bir sonraki sayfa yenilenmesinde kendi kendine otoamtik şekilde kaldırılıyor codeigniter tarafından. 
+            $this->session->set_flashdata("alert", $alert); # alert isimli bir indisim var ve bu alert isimli indisimin değeri $alert değişkeninden gelecek
+
+            # if'in içinde değil de burada redirect etmemizin sebebi alert'e verileri gönderebilmek. if'in içinde oslaydı session satırına ulaşamadan sayfa redirect edilecekti.
+            redirect(base_url("users"));
+
+        }
+        else{
+            // Hata varsa yani input doldurulmamışsa mesela, sayfa yeniden yüklenecek
+            $viewData = new stdClass();
+
+            // view'e gönderilecek değişkenlerin set edilmesi
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "password";
             $viewData->form_error = true;
 
             // Tablodan verilerin getirilmesi
@@ -309,7 +378,7 @@ class Users extends CI_Controller{ # CI -> CodeIgniter (extend etmemizin sebebi 
             );
         }
         $this->session->set_flashdata("alert", $alert);
-        redirect(base_url("references"));
+        redirect(base_url("users"));
     }
 
     public function isActiveSetter($id){
